bdf0ba270cdfcb94f6a1126d83557700c909c539
papa-react <makiya112545@gmail.com>
2021-11-18 21:35:30 -1200

    add inputs-fileds

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useLocalStorage from './custom/useLocalStorage'
 
 function App() {
+  const [key, setKey] = useState();
+  const [value, setValue] = useLocalStorage(key);
   return (
     <div>
-      <h1>Hello</h1>
+      key : <input
+              value={key}
+              onChange={(e) => { setKey(e.target.value) }}
+              type="text"
+            />
+      Value : <input
+              value={value}
+              onChange={(e) => { setValue(e.target.value) }}
+              type="text"
+            />
     </div>
   );
 }
